0bd02101bee31ad9a78f8d408a918bf4cd8ac8f3
Jacob Klimenko <droidedjake@gmail.com>
2020-11-03 23:02:41 -0500

    commit tree 495018c3c28f69f50035cbe3f6a6eb7bb33f548a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_array)
+        matches = []
+        word_array.each do |word|
+            subject = @word.split("").sort
+            comparator = word.split("").sort
+            subject == comparator ? matches << word : nil
+        end
+        matches
+    end
+end
+
